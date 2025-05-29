e9d93d0a5e025a93b3e075a75b2f8bfd4a66578e
JustinFlo <225jflores@frhsd.com>
2025-05-29 10:05:17 -0400

    Added keyboard functionality, starting to prepare NOCTI

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
+vSlider = document.querySelectorAll('.volume-slider input');
+
+let audio = new Audio(`tunes/a.wav`);
 
 const playTune = (key) => {
 
-    let audio = new Audio(`tunes/${key}.wav`);
+    audio.src = `tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key=${key}]`);
+
+    clickedKey.classList.add('active');
+
+    setTimeout(() => {
+        clickedKey.classList.remove('active');
+    }, 150);
 }
 
 pianoKeys.forEach(key => {
     key.addEventListener('click', () => playTune(key.dataset.key));
     
 }); 
+
+const pressedKey = (e) => { 
+    playTune(e.key);
+}
+
+document.addEventListener('keydown', pressedKey);
+
